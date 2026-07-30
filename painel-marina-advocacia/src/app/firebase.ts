94dc643f5999e0a6bc4ba976e56be68791501242
samyszz <samyraalves20007@gmail.com>
2026-07-30 18:13:17 -0300

    fix: corrige import do environment no firebase.ts

--- painel-marina-advocacia/src/app/firebase.ts
+++ painel-marina-advocacia/src/app/firebase.ts
@@ -1,7 +1,7 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 const app = getApps().length ? getApp() : initializeApp(environment.firebase);
 
